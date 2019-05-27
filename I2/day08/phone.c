b659e2730833924023fb049d8c78779a98cbdfdb
HyogaGlacier <hyoga_quasar@yahoo.co.jp>
2019-05-27 18:19:35 +0900

    success phone

--- I2/day08/phone.c
+++ I2/day08/phone.c
@@ -3,101 +3,84 @@
 #include <netinet/tcp.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/time.h>
-#include <sys/resource.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <assert.h>
-#include <errno.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #define TCP_STREAM SOCK_STREAM
 #define UDP_STREAM SOCK_DGRAM
+#define PACKET_SIZE 1024
 
-const int N = 44100;
+void die(char *s) {
+  perror(s);
+  exit(1);
+}
 
-enum MODE {
-  HOST,
-  GUEST,
-};
+int start_server(char **argv) {
+  char **endptr = NULL;
+  int port = strtol(argv[1], endptr, 0);
+  if (endptr != NULL) die("port is not valid");
+
+  int ss = socket(PF_INET, TCP_STREAM, 0);
+  if (ss == 1)  die("socket error");
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);
+  addr.sin_addr.s_addr = INADDR_ANY;
+  int ret = bind(ss, (struct sockaddr*)&addr, sizeof(addr));
+  if (ret == -1)  die("failed to bind");
+  ret = listen(ss, 10);
+  if (ret == -1)  die("failed to listen");
+
+  struct sockaddr_in client_addr;
+  socklen_t len = sizeof(struct sockaddr_in);
+  int s = accept(ss, (struct sockaddr*)&client_addr, &len);
+
+  close(ss);
+
+  return s;
+}
+
+int connect_server(char **argv) {
+  char *to_addr = argv[1];
+  char **endptr = NULL;
+  int to_port = strtol(argv[2], endptr, 0);
+  if (endptr != NULL) die("port is not valid");
+  int s = socket(PF_INET, TCP_STREAM, 0);
+  if (s == 1) die("socket error");
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  int ret = inet_aton(to_addr, &addr.sin_addr);
+  if (ret ==-1) die("ip address is not valid");
+  addr.sin_port = htons(to_port);
+  ret = connect(s, (struct sockaddr*)&addr, sizeof(addr));
+  if (ret == -1)  die("connect failed");
+
+  return s;
+}
 
 int main(int argc, char **argv) {
-  assert(argc == 2 || argc == 3);
   int s;
-  enum MODE mode;
   if (argc == 2) {
-    // host
-    mode = HOST;
-    char **endptr = NULL;
-    int port = strtol(argv[1], endptr, 0);
-    assert(endptr == NULL);
-
-    int ss = socket(PF_INET, TCP_STREAM, 0);
-    assert(ss != -1);
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = INADDR_ANY;
-    int ret = bind(ss, (struct sockaddr*)&addr, sizeof(addr));
-    assert(ret != -1);
-    ret = listen(ss, 10);
-    assert(ret != -1);
-
-    struct sockaddr_in client_addr;
-    socklen_t len = sizeof(struct sockaddr_in);
-    s = accept(ss, (struct sockaddr*)&client_addr, &len);
-
-    close(ss);
-    fprintf(stderr, "connected.\n");
+    s = start_server(argv);
   }
   else if(argc == 3) {
-    // guest
-    mode = GUEST;
-    char *to_addr = argv[1];
-    char **endptr = NULL;
-    int to_port= strtol(argv[2], endptr, 0);
-    fprintf(stderr, "connecting to %s:%d...\n", to_addr, to_port);
-    assert(endptr == NULL);
-    s = socket(PF_INET, TCP_STREAM, 0);
-    assert(s != -1);
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    int ret = inet_aton(to_addr, &addr.sin_addr);
-    assert(ret == 1);
-    addr.sin_port = htons(to_port);
-    ret = connect(s, (struct sockaddr*)&addr, sizeof(addr));
-    int num = errno;
-    if(ret == -1) {
-      fprintf(stderr, "print error string by strerror: %s\n", strerror(num));
-      fprintf(stderr, "print error code: %d\n", num);
-    }
-    assert(ret != -1);
-    fprintf(stderr, "connected.\n");
+    s = connect_server(argv);
   }
-  
+  else {
+    die("wrong usage");
+  }
+    
+  char *buf = (char*)malloc(sizeof(char) * PACKET_SIZE);
   while (1) {
-    short *buf = (short*)malloc(sizeof(short) * N);
-    // send
-    int n = fread(buf, sizeof(short), N, stdin);
-    if (n == 0) {
-      // EOF
-      break;
-    }
-    int m = send(s, buf, sizeof(short) * N, 0);
-    // fprintf(stderr, "send\n");
+    int n = fread(buf, sizeof(char), PACKET_SIZE, stdin);
+    int m = send(s, buf, n, 0);
+    if (n != m) die("failed to send data\n");
 
-
-    // recv
-    n = recv(s, &buf, sizeof(short) * N, 0);
-    if (n == 0) {
-      // EOF
-      break;
-    }
-    fwrite(&buf, sizeof(short), n / 2, stdout);
-    // fprintf(stderr, "recv\n");
+    n = recv(s, buf, sizeof(char) * PACKET_SIZE, 0);
+    fwrite(buf, sizeof(char), n, stdout);
   }
-
   close(s);
   return 0;
 }
