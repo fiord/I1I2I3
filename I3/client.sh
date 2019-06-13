5a193edc78205830830828fec6240ed956700489
HyogaGlacier <hyoga_quasar@yahoo.co.jp>
2019-06-13 13:04:16 +0900

    update client.sh

--- I3/client.sh
+++ I3/client.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone 192.168.100.17 50000 | play -t raw -b 16 -c 1 -e s -r 44100 -
+rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone $1 50000 | play -t raw -b 16 -c 1 -e s -r 44100 -
--- I3/cmake_install.cmake
+++ I3/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/denjo/Documents/experiment/I/I3/img
+# Install script for directory: /home/denjo/Documents/experiment/I/I3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/denjo/Documents/experiment/I/I3/img/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/denjo/Documents/experiment/I/I3/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
