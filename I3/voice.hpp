f256b71d94c98904483c95ecf84c0d8bc3e89bcd
shun2029 <tiger1998nov27@gmail.com>
2019-06-13 16:19:11 +0900

    ゼロ埋めするようにした

--- I3/voice.hpp
+++ I3/voice.hpp
@@ -5,10 +5,11 @@
 #include <string.h>
 #include <cmath>
 
+#define PACKET_SIZE 1024
 #define cut_volume 50
 
 void zero_fill(char *buf) {
-  for (int i=0; i<n; i++) {
+  for (int i=0; i<PACKET_SIZE; i++) {
     if (abs(buf[i]) < cut_volume) buf[i] = 0;
   }
 }
