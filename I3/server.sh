57770ac2708b66bb22fb5ff0d3380b9e96dc219b
HyogaGlacier <hyoga_quasar@yahoo.co.jp>
2019-06-17 16:12:14 +0900

    remove cmakefile

--- I3/server.sh
+++ I3/server.sh
@@ -4,6 +4,6 @@ rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone $1 12345 | play -t raw -b 16 -c 
 fi
 
 if [ $1 = "video" ]; then
-rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone $1 12345 50000 | play -t raw -b 16 -c 1 -e s -r 44100 - &
+rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone $1 54321 32154 | play -t raw -b 16 -c 1 -e s -r 44100 - &
   ./phone $1 12345 50000
 fi
