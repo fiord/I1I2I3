3249b8644d0bdcb45c5d1ddd6e4f4ce8f1ba04bd
HyogaGlacier <hyoga_quasar@yahoo.co.jp>
2019-06-17 16:16:08 +0900

    update port

--- I3/client.sh
+++ I3/client.sh
@@ -5,6 +5,6 @@ rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone $1 $2 12345 | play -t raw -b 16 
 fi
 
 if [ $1 = "video" ]; then
-rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone $1 $2 12345 50000 | play -t raw -b 16 -c 1 -e s -r 44100 - &
-./phone $1 $2 12345 50000
+rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone $1 $2 54321 32154 | play -t raw -b 16 -c 1 -e s -r 44100 - &
+./phone $1 $2 54321 32154
 fi
