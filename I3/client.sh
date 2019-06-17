36efaf9335a20f542b0e4aead8e980816cd17f59
HyogaGlacier <hyoga_quasar@yahoo.co.jp>
2019-06-17 13:17:56 +0900

    update server/client program

--- I3/client.sh
+++ I3/client.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone $1 $2 12345 | play -t raw -b 16 -c 1 -e s -r 44100 -
+
+if [ $1 = "sound" ]; then
+rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone $1 $2 12345 | play -t raw -b 16 -c 1 -e s -r 44100 -;
+fi
+
+if [ $1 = "video" ]; then
+rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone $1 $2 12345 50000 | play -t raw -b 16 -c 1 -e s -r 44100 -;
+fi
--- I3/server.sh
+++ I3/server.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone $1 12345 | play -t raw -b 16 -c 1 -e s -r 44100 -
+if [ $1 = "sound" ]; then
+rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone $1 12345 | play -t raw -b 16 -c 1 -e s -r 44100 -;
+fi
+
+if [ $1 = "video" ]; then
+rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone $1 12345 50000 | play -t raw -b 16 -c 1 -e s -r 44100 -;
+fi
