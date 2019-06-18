0072cd8aadf1f3ae40123b046df127e4cef5aec4
HyogaGlacier <hyoga_quasar@yahoo.co.jp>
2019-06-18 14:44:15 +0900

    avoid dual define of packet_size

--- I3/video.cpp
+++ I3/video.cpp
@@ -1,7 +1,7 @@
 #include "video.hpp"
 #include "connect.hpp"
 
-#define PACKET_SIZE 65500
+#define PACKET_VIDEO_SIZE 65500
 #define HEIGHT 480
 #define WIDTH 640
 
@@ -17,7 +17,7 @@ void send_video(int s) {
   cv::Mat img;
   std::vector<unsigned char> ibuff;
   std::vector<int> param = { CV_IMWRITE_JPEG_QUALITY, 85 };
-  char buff[PACKET_SIZE];
+  char buff[PACKET_VIDEO_SIZE];
 
   while(1) {
     if (!cap.read(img)) {
@@ -25,13 +25,13 @@ void send_video(int s) {
     }
 
     cv::imencode(".jpg", img, ibuff, param);
-    if (ibuff.size() < PACKET_SIZE) {
-      for (int i = 0; i < PACKET_SIZE; i++) {
+    if (ibuff.size() < PACKET_VIDEO_SIZE) {
+      for (int i = 0; i < PACKET_VIDEO_SIZE; i++) {
         if (i < ibuff.size()) buff[i] = ibuff[i];
         else  buff[i] = 0;
       }
-      int m = send(s, buff, PACKET_SIZE, 0);
-      if (m != PACKET_SIZE)  die("failed to send img data");
+      int m = send(s, buff, PACKET_VIDEO_SIZE, 0);
+      if (m != PACKET_VIDEO_SIZE)  die("failed to send img data");
     }
     ibuff.clear();
   }
@@ -41,12 +41,12 @@ void recv_video(int s) {
   cvNamedWindow("tvphone", CV_WINDOW_AUTOSIZE);
 
   cv::Mat img = cv::Mat(HEIGHT, WIDTH, CV_8UC3);
-  char buff[PACKET_SIZE];
+  char buff[PACKET_VIDEO_SIZE];
   std::vector<unsigned char> ibuff;
 
   while(1) {
     // 受信・表示
-    int m = recv(s, buff, PACKET_SIZE, 0);
+    int m = recv(s, buff, PACKET_VIDEO_SIZE, 0);
     fprintf(stderr, "[info] recv: recv_size=%d\n", m);
     for (int i = 0; i < sizeof(buff); i++) {
       ibuff.push_back((unsigned char)buff[i]);
